53aad8291bfa68965c7153c2f4a61fa074d511d6
zhangyun <291842650@qq.com>
2019-07-22 18:13:26 +0800

    update

--- routes/users.js
+++ routes/users.js
@@ -21,16 +21,16 @@ router.get('/isLogin', function(req, res, next) {
 });
 
 /**用户账号登录*/
-router.get('/login', function(req, res, next) {
+router.post('/login', function(req, res, next) {
         // 1.手机登录 2.账号密码登录  3 微信第三方登录 
         var sql,msg= "";
-        var state = req.query.state;
+        var state = req.body.state;
         if(state==1){
-          var {phone,code} = req.query;
-          sql = `select t_user.nick_name,t_user.icon,t_user.icon,t_user.member_id,t_user.signature,t_user.is_freeze from t_icode left join t_user on t_icode.phone = t_user.phone where t_icode.phone = "${phone}" and  t_icode.code = "${code}"`;
+          var {phone,code} = req.body;
+          sql = `select t_user.nick_name,t_user.token,t_user.icon,t_user.icon,t_user.member_id,t_user.signature,t_user.is_freeze from t_icode left join t_user on t_icode.phone = t_user.phone where t_icode.phone = "${phone}" and  t_icode.code = "${code}"`;
           msg = "验证码错误";
         }else if(state==2){
-          var {account,password} = req.query;
+          var {account,password} = req.body;
           var password = uuidv5(password, uuidv5.DNS);
           msg = "账号或密码错误";
           sql = `select id from t_user where account = "${account}" and password = "${password}"`;
@@ -42,7 +42,7 @@ router.get('/login', function(req, res, next) {
         console.log(sql);
         conf.query(sql,function(err,result){
         if(result.length){
-          res.json({code:200,data:result[0]});
+          res.json({code:200,data:result[0],msg:"登录成功"});
         }else{
           res.json({code:-1,msg: msg});
         }

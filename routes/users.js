063de8a38144fcab74c78bdba46fcd589f0280a4
zhangyun <291842650@qq.com>
2019-09-06 16:22:17 +0800

    完善用户粉丝列表

--- routes/users.js
+++ routes/users.js
@@ -111,15 +111,19 @@ router.post('/changeUserInfo',checklogin.AuthMiddleware, function(req, res, next
 router.get('/fans',checklogin.AuthMiddlewareGet, function(req, res, next) {
   var {token,page} = req.query;
   var offSets = ((!isNaN(page)&&page>0?page:1)- 1) * 10;
-  var sqlFans = `select t_user.nick_name,t_focus.is_focus  from t_fans  
+  var sqlFans = `select t_user.nick_name,t_user.signature,t_user.icon,t_focus.is_focus  from t_fans  
             left join t_user on t_fans.user_fans = t_user.id 
             left join t_focus on t_fans.user_fans = t_focus.focus_user 
             and t_focus.user_id = (select id from t_user where token = "${token}") 
             where t_fans.user_id = (select id from t_user where token = "${token}") and t_fans.is_fans = 1
-            order by t_fans.create_time desc limit 10 offset ${offSets}`;
-        conf.query(sqlFans,function(err,result){
-          res.json({code: 200,data: result});
-        });
+            order by t_fans.update_time desc limit 10 offset ${offSets}`;
+            conf.query(sqlFans,function(err,result){
+              if(result.length){
+                res.json({code: 200,data: result});
+              }else{
+                res.json({code: -1});
+              }
+            });
 });
 
 

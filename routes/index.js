41631918417327a12eaa042fa465f82ae311e2e8
Ninjacx <291842650@qq.com>
2019-12-23 21:02:00 +0800

    20191223

--- routes/index.js
+++ routes/index.js
@@ -92,7 +92,7 @@ router.get('/qnyToken', function(req, res, next) {
 	var secretKey = 'cRdesdlbE78qTlmwwdE0joQO-MViCgsVeccH2-7D';
 	var mac = new qiniu.auth.digest.Mac(accessKey, secretKey);	 
 	var options = {
-		scope: "tour1",
+		scope: "tour3",
 		// callbackUrl: 'http://192.168.1.39/QNYcallback',
 		// callbackBody: '{"key":"$(key)","hash":"$(etag)","fsize":$(fsize),"bucket":"$(bucket)","name":"$(x:name)"}',
 	  };
@@ -186,7 +186,7 @@ router.post('/uploadQNY',function(req, res, next) {
 					for (let j = 0; j < TextArr.length; j++) {
 						if(ImgTextId[index] == j){
 							console.log(TextArr[j].txtImgId);
-							insertStr+=`("http://q168j3r86.bkt.clouddn.com/${fileName}${imagePath}","${TextArr[j].txtImgId}","${contentId}"),`;
+							insertStr+=`("http://q2ytkxvzo.bkt.clouddn.com/${fileName}${imagePath}","${TextArr[j].txtImgId}","${contentId}"),`;
 						}
 					}
 					// 上传到七牛云

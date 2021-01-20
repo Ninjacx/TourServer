ede5eb783bc550eeb90aef33bbdb2daf8e65b936
sky <zhang.yun@ctgtmo.com>
2021-01-20 18:37:51 +0800

    sequelizeDb

--- conf/SequelizeDb.js
+++ conf/SequelizeDb.js
@@ -1,4 +1,4 @@
-const {Sequelize,DataTypes,Model} = require('sequelize');
+const {Sequelize,DataTypes,Model,QueryTypes} = require('sequelize');
 
 //Setting up a connection
 const sequelize = new Sequelize('Tour','root','123',{
@@ -40,17 +40,41 @@ User.init({
   // }
 }, {
   // 这是其他模型参数
+  timestamps: true,
+
+  // 不想要 createdAt
+  createdAt: false,
+  updatedAt: false,
   tableName: 't_user',
   sequelize, // 我们需要传递连接实例
   modelName: 'User' // 我们需要选择模型名称
 });
 
- User.findAll({
-  attributes: ['nick_name', 'token','signature']
-}).then(function(res){
-  // console.log('res',res);
-  // JSON.parse()
-  // JSON.stringify()
-});
+// async function aa (){
+//   const records = await sequelize.query('select * from t_user', {
+//     nest: true,
+//     type: QueryTypes.SELECT
+//   });
+//   console.log(records.length);
+// }
+// aa()
+
+ async function bb (){
+  var res = await User.findOne({attributes: ['*'], where: { nick_name: '旅行的乌龟12' } })
+  console.log('res',JSON.parse(JSON.stringify(res)));
+  //  .then(function(res){
+  //   console.log('res',JSON.parse(JSON.stringify(res)).length );
+  //   JSON.parse(JSON.stringify(res)).map((item)=>{
+  //     console.log('item.nick_name',item.nick_name);
+  //   })
+  // });
+  // const records = await sequelize.query('select * from t_user', {
+  //   nest: true,
+  //   type: QueryTypes.SELECT
+  // });
+  // console.log(records.length);
+}
+bb()
+
 
 // exports.sequelize = sequelize
